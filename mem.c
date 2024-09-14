03d516ab0ba1e3f632ccb8599a64198e6b21846e
hamza wahed <hzd454@gmail.com>
2024-09-14 19:41:38 -0300

    Create fm_index struct

---
+++ mem.c
@@ -0,0 +1,9 @@
+#include "fm_index.h"
+#include <stdlib.h>
+#include <string.h>
+
+#define MEM_LENGTH 40
+
+int MEMs(char *pattern) {
+  int *stamps = (int *)malloc(sizeof(int) * strlen(pattern));
+}
