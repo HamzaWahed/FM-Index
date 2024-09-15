09f3084f6393aa7869bba9f8c93e7e086f32ffee
hamza wahed <hzd454@gmail.com>
2024-09-15 19:16:01 -0300

    fix mem finder

--- mem.c
+++ mem.c
@@ -4,52 +4,58 @@
 #include <string.h>
 
 #define MEM_LENGTH 40
+#define TEXT_LENGTH 1000000
 
-long long MEMs(char *file, char *rev_file, char *pattern) {
+uint64_t MEMs(char *file, char *rev_file, char *pattern) {
   fm_index *fm = build(file);
   fm_index *rev_fm = build(rev_file);
-  long long mem_counts = 0;
-  char *tmp = malloc(strlen(pattern));
+  uint64_t mem_counts = 0, pattern_index, current_index = 0;
+  char *tmp = malloc(MEM_LENGTH);
 
-  for (int i = 0; i <= strlen(pattern);) {
-    int j = i + MEM_LENGTH;
-    strncpy(tmp, pattern, j);
+  while (current_index <= strlen(pattern)) {
+    int s = 0;
+    int e = TEXT_LENGTH - 1;
+    strncpy(tmp, pattern, current_index + MEM_LENGTH);
 
-    while (count(rev_fm, tmp) && j <= strlen(pattern)) {
-      j++;
-      strncpy(tmp, pattern, j);
-    }
+    if (count(fm, tmp)) {
+      for (pattern_index = current_index + MEM_LENGTH + 1;
+           pattern_index < strlen(pattern) && s <= e; pattern_index++) {
+        s = rev_fm->C_array[char_to_nucleotide(pattern[pattern_index])] +
+            rank(rev_fm, char_to_nucleotide(pattern[pattern_index]), s - 1) + 1;
+        e = rev_fm->C_array[char_to_nucleotide(pattern[pattern_index])] +
+            rank(rev_fm, char_to_nucleotide(pattern[pattern_index]), e);
+      }
 
-    if (j > i + MEM_LENGTH) {
       mem_counts++;
     }
 
-    int k = 0;
-    strncpy(tmp, pattern + j + 1, 1);
-    while (count(fm, tmp) && (j + 1 - k) >= 0) {
-      k++;
-      strncpy(tmp, pattern + j + 1 - k, k + 1);
+    if (pattern_index == strlen(pattern)) {
+      break;
     }
 
-    if (k > 0 && strlen(tmp) > MEM_LENGTH) {
-      mem_counts++;
+    s = 0;
+    e = TEXT_LENGTH - 1;
+    for (current_index = pattern_index + 1; current_index >= 0 && s <= e;
+         current_index--) {
+      s = fm->C_array[char_to_nucleotide(pattern[current_index])] +
+          rank(fm, char_to_nucleotide(pattern[current_index]), s - 1) + 1;
+      e = fm->C_array[char_to_nucleotide(pattern[current_index])] +
+          rank(fm, char_to_nucleotide(pattern[current_index]), e);
     }
-
-    i = j - k + 1;
   }
 
+  free(tmp);
   free_fm_index(fm);
   free_fm_index(rev_fm);
-  free(tmp);
   return mem_counts;
 }
 
 int main(int argc, char **argv) {
   FILE *pattern_file = fopen(argv[3], "r");
-  size_t pattern_length = 10000;
-  char *pattern = malloc(pattern_length);
+  size_t max_pattern_length = 10000;
+  char *pattern = malloc(max_pattern_length);
 
-  getline(&pattern, &pattern_length, pattern_file);
+  getline(&pattern, &max_pattern_length, pattern_file);
   printf("mem counts: %lld", MEMs(argv[1], argv[2], pattern));
 
   free(pattern);
